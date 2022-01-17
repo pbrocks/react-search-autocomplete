43c73d890cb53dc2e9070e1ac7cb0c2e926518bf
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-01-17 10:29:23 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
 import React from "react";
 import "./App.css";
-import logo from "./sickdoodle.png";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
+// import movieItems from "./components/movieItems";
+// https://stackoverflow.com/questions/54064909/redirect-to-not-redirecting-to-the-desired-url-react?rq=1
+import theData from "./data/small-sample.json";
 
 function App() {
-  const items = [
-    {
-      id: 0,
-      name: "Cobol",
-    },
-    {
-      id: 1,
-      name: "JavaScript",
-    },
-    {
-      id: 2,
-      name: "Basic",
-    },
-    {
-      id: 3,
-      name: "PHP",
-    },
-    {
-      id: 4,
-      name: "Java",
-    },
-  ];
 
-  const manyItems = [...new Array(10000)].map((_, i) => ({
-    id: i,
-    name: `something${i}`,
-    description:
-      "Some description text, where the search will be performed too.",
-  }));
-
-  const movieItems = [
-    {
-      id: 0,
-      title: "Titanic",
-      description: "A movie about love",
-    },
-    {
-      id: 1,
-      title: "Dead Poets Society",
-      description: "A movie about poetry and the meaning of life",
-    },
-    {
-      id: 2,
-      title: "Terminator 2",
-      description: "A robot from the future is sent back in time",
-    },
-    {
-      id: 3,
-      title: "Alien 2",
-      description: "Ripley is back for a new adventure",
-    },
-  ];
+const movieItems =  theData;
 
   const handleOnSearch = (string, results) => {
     console.log(string, results);
@@ -80,48 +32,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ width: 200, margin: 20 }}>
-          <img
-            src={logo}
-            alt="logo"
-            style={{ width: "100%", marginBottom: 20 }}
-          />
-          <div style={{ marginBottom: 20 }}>Try to type "JavaScript"</div>
-          <ReactSearchAutocomplete
-            items={items}
-            onSearch={handleOnSearch}
-            onHover={handleOnHover}
-            onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
-            onClear={handleOnClear}
-            styling={{ zIndex: 2 }} // To display it on top of the search box below
-            autoFocus
-          />
-          <div style={{ marginTop: 20 }}>This text will be covered!</div>
-        </div>
-        <div style={{ width: 200, margin: 20 }}>
-          <h2>10000 items!</h2>
-          <div style={{ marginBottom: 20 }}>Try to type a number</div>
-          <ReactSearchAutocomplete
-            items={manyItems}
-            maxResults={15}
-            onSearch={handleOnSearch}
-            onHover={handleOnHover}
-            onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
-            onClear={handleOnClear}
-            fuseOptions={{ keys: ["name", "description"] }} // Search in the description text as well
-            styling={{ zIndex: 1 }} // To display it on top of the search box below
-          />
-          <div style={{ marginTop: 20 }}>This text will be covered!</div>
-        </div>
-        <div style={{ width: 200, margin: 20 }}>
-          <h2>My custom searchbox!</h2>
-          <div style={{ marginBottom: 20 }}>Try to type "Titanic"</div>
+        <div style={{ width: 400, margin: 20 }}>
+          <h2>Pharmacy Lookup!</h2>
           <ReactSearchAutocomplete
             items={movieItems}
-            fuseOptions={{ keys: ["title", "description"] }} // Search on both fields
-            resultStringKeyName="title" // String to display in the results
+            fuseOptions={{ keys: ["label", "PharmacyName"] }} // Search on both fields
+            resultStringKeyName="label" // String to display in the results
             onSearch={handleOnSearch}
             onHover={handleOnHover}
             onSelect={handleOnSelect}
@@ -136,7 +52,7 @@ function App() {
               boxShadow: "none",
               hoverBackgroundColor: "lightgreen",
               color: "darkgreen",
-              fontSize: "12px",
+              fontSize: "1rem",
               fontFamily: "Courier",
               iconColor: "green",
               lineColor: "lightgreen",
---
+++ src/components/movieItems.js
@@ -0,0 +1,24 @@
+const movieItems = [
+    {
+        id: 0,
+        title: "Titanic",
+        description: "A movie about love",
+    },
+    {
+        id: 1,
+        title: "Dead Poets Society",
+        description: "A movie about poetry and the meaning of life",
+    },
+    {
+        id: 2,
+        title: "Terminator 2",
+        description: "A robot from the future is sent back in time",
+    },
+    {
+        id: 3,
+        title: "Alien 2",
+        description: "Ripley is back for a new adventure",
+    },
+];
+
+export default movieItems;
